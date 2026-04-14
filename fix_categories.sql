ad94cb0e7583040ba9f288bd01019c51be8c4afd
Fabian Espejo <fespejo@plm.com.co>
2026-04-13 20:46:43 -0500

    chore: add fix categories script

---
+++ fix_categories.sql
@@ -0,0 +1,6 @@
+UPDATE categories SET name='5. Bíceps' WHERE name LIKE '5. B%';
+UPDATE categories SET name='6. Tríceps' WHERE name LIKE '6. Tr%';
+UPDATE categories SET name='7. Glúteos' WHERE name LIKE '7. Gl%';
+UPDATE categories SET name='8. Isquiosurales' WHERE name LIKE '8. Izq%' OR name LIKE '8. Isq%';
+UPDATE categories SET name='10. Pantorrillas' WHERE name LIKE '10. Pant%';
+UPDATE categories SET name='11. Abdomen' WHERE name LIKE '11. Abd%';
